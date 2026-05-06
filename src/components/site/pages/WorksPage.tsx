735686cc5c00a11d491713416b5d6328670abba4
kageki128 <kagemakopf@gmail.com>
2026-05-06 20:16:48 +0900

    モーダル表示中にスクロールバーの幅を予約するようにした

--- src/components/site/pages/WorksPage.tsx
+++ src/components/site/pages/WorksPage.tsx
@@ -140,19 +140,13 @@ export default function WorksPage({ featuredWorks, allWorksByYear }: WorksPagePr
     const { body, documentElement } = document;
     const previousBodyOverflow = body.style.overflow;
     const previousHtmlOverflow = documentElement.style.overflow;
-    const previousBodyPaddingRight = body.style.paddingRight;
-    const scrollbarWidth = window.innerWidth - documentElement.clientWidth;
 
     body.style.overflow = "hidden";
     documentElement.style.overflow = "hidden";
-    if (scrollbarWidth > 0) {
-      body.style.paddingRight = `${scrollbarWidth}px`;
-    }
 
     return () => {
       body.style.overflow = previousBodyOverflow;
       documentElement.style.overflow = previousHtmlOverflow;
-      body.style.paddingRight = previousBodyPaddingRight;
     };
   }, [selectedWork]);
 
