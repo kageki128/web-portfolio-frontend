be174f28b0ee5701b00e64d1baa06fe1271f8ed3
kageki128 <kagemakopf@gmail.com>
2026-05-07 03:47:29 +0900

    フッターの日付のフォーマットを修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const now = new Date();
-  const lastUpdated = `${now.getFullYear()}/${String(now.getMonth() + 1).padStart(2, "0")}/${String(
+  const lastUpdated = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-${String(
     now.getDate(),
   ).padStart(2, "0")}`;
 
